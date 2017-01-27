4decf7e6724171de8fa89f06472cf626d017942b
Bruno <valinoti@inti.gob.ar>
2017-01-27 12:14:30 -0300

    examples/lattice_icestick: added testbench for clock example

--- examples/lattice_icestick/clock/top.v
+++ examples/lattice_icestick/clock/top.v
@@ -13,7 +13,10 @@ module top (
    input wire     rst_i,
 //Salidas
    output wire    led1_o,
-   output wire    led2_o
+   output wire    led2_o,
+   output wire    led3_o,
+   output wire    led4_o,
+   output wire    led5_o
    );
 
 //Signals declaration sector
@@ -21,18 +24,21 @@ wire led;
 
 //Modules instantiation
    Blink #(
-      .FREQUENCY(12E6),
+      .FREQUENCY(12_000_000),
       .SECONDS  (1)          
    )
    blink(
       .clk_i   (clk_i),
       .rst_i   (rst_i),
-      .blink_o (led1)
+      .blink_o (led)
    );
 
 //Concurrent assigns
 assign led1_o = led;
-assign led2_o = ~led;
+assign led2_o = led;
+assign led3_o = led;
+assign led4_o = led;
+assign led5_o = ~led;
 
 endmodule
 
---
+++ examples/lattice_icestick/clock/top_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 100ps
+module top_tb ();
+reg clk  = 1'b0     ;
+reg rst  = 1'b1     ;
+wire LED1, LED2, LED3, LED4, LED5;
+
+top DUT(
+//Entradas
+   .clk_i(clk), 
+   .rst_i(rst),
+//Salidas
+   .led1_o(LED1),
+   .led2_o(LED2),
+   .led3_o(LED3),
+   .led4_o(LED4),
+   .led5_o(LED5)
+   );
+
+always #1 clk = ~clk;
+
+initial begin
+   $dumpfile("top_tb.vcd");
+   $dumpvars(0, top_tb);
+   #4 rst = 1'b0;
+   @(posedge(clk));
+   if (LED5 == 1'b1)
+      $display("Inicializacion OK."); 
+   else
+      $display("Mala inicializacion");
+	 
+    
+   repeat(12_000_000) @(posedge(clk));
+   repeat(2) @(posedge(clk));
+   if (LED5 == 1'b0)
+      $display("termino OK.");
+   else
+      $display("Termino mal");
+	    
+   $finish;
+end
+
+ initial
+    begin
+       $monitor("%b",LED1);
+ end
+
+endmodule
